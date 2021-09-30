6d1be22fbe12a76ee3ed7b043492acabd6b608bf
ShariqAli-Dev <shariqali.dev@gmail.com>
2021-09-29 20:38:57 -0400

    completed post and get for /api/users

--- api/server.js
+++ api/server.js
@@ -9,8 +9,35 @@ const server = express();
 server.use(express.json());
 
 // GETS
+server.get('/api/users', (req, res) => {
+  users
+    .find()
+    .then((users) => res.status(500).json(users))
+    .catch((err) =>
+      res
+        .status(500)
+        .json({ message: 'The user with the specified ID does not exist' })
+    );
+});
 
 //POST
+server.post('/api/users', (req, res) => {
+  const newUser = req.body;
+
+  if (!newUser.name || !newUser.bio)
+    res
+      .status(400)
+      .json({ message: 'Please provide name and bio for the user' });
+
+  users
+    .insert(newUser)
+    .then((insertedUser) => res.status(201).json(insertedUser))
+    .catch((err) =>
+      res.status(500).json({
+        message: 'There was an error while saving the user to the database',
+      })
+    );
+});
 
 // POST
 
